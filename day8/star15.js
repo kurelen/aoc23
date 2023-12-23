485a978061d87bbdda15f254fe89b2389cca2d65
kurelen <kurelen@googlemail.com>
2023-12-23 15:28:11 +0100

    Solve star 14

---
+++ day8/star15.js
@@ -0,0 +1,106 @@
+const fs = require('node:fs');
+const readline = require('node:readline')
+
+const line_r = /(\w+) = \((\w+), (\w+)\)/g;
+
+function walker(directions) {
+  let i = 0;
+  let counter = 0;
+  return {
+    steps() {
+      return counter;
+    },
+    next() {
+      const result = directions[i];
+      counter++;
+      i++;
+      if (i >= directions.length) {
+        i = 0;
+      }
+      return result;
+    }
+  }
+}
+
+function generate_parser() {
+  const directions = [];
+  const maps = {};
+  let finished = false;
+  return {
+    parse_line(line) {
+      if (finished) {
+        throw new Error("parser finished");
+      }
+      if (line === '') {
+        return;
+      }
+      if (directions.length === 0) {
+        directions.push(...line);
+        Object.freeze(directions);
+        return;
+      }
+      const match = [...line.matchAll(line_r)];
+      const [[_, key, l, r]] = match;
+      if (!Object.hasOwnProperty(key)) {
+        maps[key] = [l, r];
+        return;
+      }
+      throw new Error("Duplicate key", { cause: key });
+    },
+    finish() {
+      finished = true;
+      Object.freeze(maps);
+    },
+    directions,
+    maps,
+  };
+}
+
+async function process_file(file, parser) {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(file),
+    crlfDelay: Infinity,
+  });
+  for await (const line of rl) {
+    parser.parse_line(line);
+  }
+  parser.finish();
+  return parser;
+}
+
+const parser = generate_parser();
+
+function ends_with(char) {
+  return function key_ends_with(key) {
+    return key.endsWith(char);
+  }
+}
+
+function negate(predicate_fn) {
+  return function negated(...args) {
+    return !predicate_fn(...args);
+  }
+}
+
+process_file('input', parser)
+  .then((p) => {
+    const w = walker(p.directions);
+    const m = p.maps
+    let keys = Object.keys(p.maps).filter(ends_with('A'));
+    let steps = 0;
+    const not_ends_with_Z = negate(ends_with('Z'));
+    while (keys.some(not_ends_with_Z)) {
+      const d = w.next();
+      const new_keys = keys.map(key => {
+        const [l, r] = m[key]; 
+        return d === 'L' ? l : r
+      })
+      keys = new_keys;
+      steps++;
+      if (steps % 1000000 === 0) {
+        console.log(steps)
+      }
+    }
+    console.log(`Required steps from 'AAA' to 'ZZZ': ${w.steps()}`);
+
+  });
